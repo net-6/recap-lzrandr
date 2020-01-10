8cc66451fe11c1eaa1e7ffb27853279bda6a00cf
Andrei L <lazar.andrei@outlook.com>
2020-01-10 23:28:10 +0200

    6

---
+++ week5-recap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week5_recap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
